6410a3ae3ea9e3a30374babc04499c89aec78f16
juliooo2 <laforsej@gmail.com>
2026-05-15 10:40:47 +0200

    Update tennis-data.js

--- api/tennis-data.js
+++ api/tennis-data.js
@@ -23,6 +23,7 @@ export default async function handler(req, res) {
     'getPlayerMatchFilters',
     'getH2HFixtures',
     'getDateFixtures',
+    'getDateRangeFixtures',
     'getPlayerRanking'
   ];
   if (allowed.indexOf(endpoint) === -1) {
